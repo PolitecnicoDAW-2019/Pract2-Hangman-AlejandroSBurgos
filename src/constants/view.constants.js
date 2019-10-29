c502158a9d3204fd10460d049a740aa70c2bd821
alejandrosb <alejandro.sanchez.burgos@gmail.com>
2019-10-29 22:04:12 +0100

    build(constants): inserted the constants necessary for the view

---
+++ src/constants/view.constants.js
@@ -0,0 +1,7 @@
+const GUI = {
+  canvas: window.document.getElementById('canvas'),
+  restart: window.document.getElementById('restart'),
+  expectedWord: window.document.getElementById('expectedWord'),
+  divCharacters: window.document.getElementById('divCharacters'),
+  characters: window.document.getElementsByClassName('characters')
+};
